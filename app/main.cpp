bc2bf94c567939ae25d1d2f39867ad9e06b79efe
Rishabh Choudhary <rishchou@umd.edu>
2018-10-09 12:24:23 -0700

    [#9 : RMV] Removed existing test cases in test.cpp and lib.hpp dependency in app/main.cpp

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-#include <lib.hpp>
-
+#include "../include/laneDetector.hpp"
 int main()
 {
-    dummy();
     return 0;
 }
--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,2 @@
 #include <gtest/gtest.h>
 
-TEST(dummy, should_pass) {
-  EXPECT_EQ(1, 1);
-}
