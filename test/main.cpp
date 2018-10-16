747b992f882438be14600d9a14577a2db259014f
Rishabh Choudhary <rishchou@umd.edu>
2018-10-15 22:59:29 -0700

    [#9 : RMV] Edited test files to exclude old dependencies

--- test/main.cpp
+++ test/main.cpp
@@ -1,3 +1,13 @@
+/**============================================================================
+ * @file         main.cpp
+ * Author      : Rishabh Choudhary, Akash Atharv
+ * Version     : 1.0
+ * @Copyright  : MIT License
+ * Copyright 2018 Rishabh Choudhary, Akash Atharv
+ * @brief        file for unit test cases of googletest
+ *============================================================================
+ */
+
 #include <gtest/gtest.h>
 
 int main(int argc, char** argv) {
--- test/test.cpp
+++ test/test.cpp
@@ -1,2 +1,16 @@
-#include <gtest/gtest.h>
+/**============================================================================
+ * @file         test.cpp
+ * Author      : Rishabh Choudhary, Akash Atharv
+ * Version     : 1.0
+ * @Copyright  : MIT License
+ * Copyright 2018 Rishabh Choudhary, Akash Atharv
+ * @brief        test file for unit testing
+ *============================================================================
+ */
 
+#include <gtest/gtest.h>
+/*
+TEST(dummy, should_pass) {
+  EXPECT_EQ(1, 1);
+}
+*/
