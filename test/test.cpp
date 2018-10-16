8889c11439f521061436d1d4a65b7a1d1646affd
akashatharv <akashatharv@gmail.com>
2018-10-16 12:25:37 -0400

    [#17 :TST] Preliminary test cases are implemented to check the heading prediction in different frames of the sample video

--- test/test.cpp
+++ test/test.cpp
@@ -8,9 +8,64 @@
  *============================================================================
  */
 
+#include <iostream>
+#include <string>
+#include <vector>
+#include "opencv2/opencv.hpp"
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include "../include/imageProcessor.hpp"
+#include "../include/lanes.hpp"
 #include <gtest/gtest.h>
-/*
-TEST(dummy, should_pass) {
-  EXPECT_EQ(1, 1);
+
+std::string testHeading(int frameNumber)
+{
+      lanes img;
+      cv::Mat originalImage;
+      cv::Mat maskImage;
+      cv::Mat frame;
+      cv::Mat separateImage;
+      std::vector<cv::Vec4i> lines;
+      std::vector<cv::Point> rightPts;
+      std::vector<cv::Point> leftPts;
+      std::vector<cv::Point> outputLines;
+
+      cv::VideoCapture cap("../data/Vehicle Detection Raw Video.mp4");
+      cap.set(cv::CAP_PROP_POS_FRAMES, frameNumber);
+      cap.read(frame);
+      img.setOriginalImage(frame);
+      img.rgbToGray(frame);
+      img.noiseFilter(img.getGrayImage());
+      img.edgeDetector(img.getNoiseImage());
+      cv::Mat mask = cv::Mat::zeros(img.getEgdeImage().size(), img.getEgdeImage().type());
+      cv::Point pts[4] = {
+        cv::Point(0, 831),
+        cv::Point(629, 465),
+        cv::Point(756, 472),
+        cv::Point(1195, 711)
+    };
+      cv::fillConvexPoly(mask, pts, 4, cv::Scalar(255, 0, 0));
+      cv::bitwise_and(img.getEgdeImage(), mask, maskImage);
+      img.grayToRGB(img.getEgdeImage());
+      lines = img.houghTransform(maskImage, img.getHoughImage());
+      separateImage = img.lineSeparation(lines, img.getEgdeImage());
+      outputLines = img.fitLine(rightPts, leftPts);
+      return(img.prediction);
 }
-*/
+
+TEST(LaneHeadingTest, Vehicle_go_straight) {
+      EXPECT_EQ(testHeading(100),"Straight");
+}
+
+TEST(LaneHeadingTest, Vehicle_right_turn) {
+      EXPECT_EQ(testHeading(600),"Right Turn");
+}
+
+TEST(LaneHeadingTest, Vehicle_left_turn) {
+      EXPECT_EQ(testHeading(1),"Left Turn");
+}
+
+
+
